041bd0aa67761911165d230c752ff06ec5c06431
neilyueh <neil.yueh@gmail.com>
2017-01-12 14:48:29 +0800

    add chinatrust python code

---
+++ gen_captcha_chinatrust_python/gen.py
@@ -0,0 +1,10 @@
+﻿# 成功範例
+import requests
+import shutil
+
+rs=requests.session()
+res=rs.get('https://www.ctbcbank.com/CTCBPortalWeb/servlets/LoginCaptchaServlet', stream=True) # ,verify=False
+f=open("check.jpeg","wb")
+f.write(res.content)
+f.close()
+
